295f27a46d9973b60c9783075b66135887c74aa8
jeffmour4 <jefflbmoura@gmail.com>
2025-06-21 20:25:33 -0300

    add assets, classes and menu

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
+Invaders Zone
+
+It is a game which you fight against enemies that want to take your territory
+You need to take them away
+
+
 Game made with python
+
+
---
+++ code/Game.py
@@ -0,0 +1,17 @@
+import pygame
+from code.Menu import Menu
+from code.Const import WINDOW_WIDTH, WINDOW_HEIGHT
+
+
+class Game:
+    def __init__(self):
+        pygame.init()
+        self.window = pygame.display.set_mode(size=(WINDOW_WIDTH, WINDOW_HEIGHT))
+
+    def run(self):
+        while True:
+            menu = Menu(self.window)
+            menu.run()
+            pass
+
+
