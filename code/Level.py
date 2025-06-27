618590ccdb9aaf388d7ce49b2cc8a25f0ee727c7
jeffmour4 <jefflbmoura@gmail.com>
2025-06-27 16:14:20 -0300

    add features to menu and first level

--- code/Level.py
+++ code/Level.py
@@ -1,5 +1,10 @@
-import pygame.display
+import sys
+from pydoc_data.topics import topics
 
+import pygame.display
+from pygame import Rect
+
+from code.Const import COLOR_WHITE, WINDOW_HEIGHT, COLOR_ORANGE
 from code.Entity import Entity
 from code.EntityFactory import EntityFactory
 
@@ -11,12 +16,34 @@ class Level:
         self.name = name
         self.entity_list: list[Entity] = []
         self.entity_list.extend(EntityFactory.get_entity('Level1Bg'))
+        self.timeout = 20000 # 20 seconds
 
     def run(self):
+        pygame.mixer_music.load('./assets/Sounds/Battle Music WAV.wav')
+        pygame.mixer_music.play(-1)
+        clock = pygame.time.Clock()
         while True:
+            clock.tick(30)
             for ent in self.entity_list:
                 self.window.blit(source=ent.surf, dest=ent.rect)
                 ent.move()
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+
+            # printed text
+            self.level_text(14, f'{self.name} - Timeout: {self.timeout /1000 :.1f}s', COLOR_ORANGE, (10, 5))
+            self.level_text(14, f'fps: {clock.get_fps() :.0f}', COLOR_WHITE, (10, WINDOW_HEIGHT -35))
+            self.level_text(14, f'entidades: {len(self.entity_list)}', COLOR_WHITE, (10, WINDOW_HEIGHT - 20))
             pygame.display.flip()
         pass
+
+    def level_text(self, text_size: int, text: str, text_color: tuple, text_pos: tuple):
+        text_font: Font = pygame.font.SysFont(name="Gentium Book Basic", size=text_size)
+        text_surf: Surface = text_font.render(text, True, text_color).convert_alpha()
+        text_rect: Rect = text_surf.get_rect(left=text_pos[0], top=text_pos[1])
+        self.window.blit(source=text_surf, dest=text_rect)
+
+
 
--- code/Menu.py
+++ code/Menu.py
@@ -9,7 +9,7 @@ class Menu:
 
     def __init__(self, window):
         self.window = window
-        self.surf = pygame.image.load('./assets/menubg.png')
+        self.surf = pygame.image.load('./assets/menubg.png').convert_alpha()
         self.rect = self.surf.get_rect(left=0, top=0)
 
     def run(self, ):
